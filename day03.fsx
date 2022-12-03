d1f9ada2aaa37f127eba246d69e8cdf2e9cb4c0e
Rinorragi <rinorragi@gmail.com>
2022-12-03 14:39:48 +0200

    Day3 small refactor

--- day03.fsx
+++ day03.fsx
@@ -11,22 +11,20 @@ let charToIntValue (c : char) =
     | false -> int c - int 'A' + 27
 
 let ruckSackInput = 
-    System.IO.File.ReadAllText "./input/day03.txt"
-    // Replace CRLF to only LF (copy+paste and input in different format)
-    |> fun s -> s.Replace("\r\n", nl)
-    // Rucksack
-    |> fun s -> s.Split(nl, StringSplitOptions.RemoveEmptyEntries)
+    System.IO.File.ReadAllLines "./input/day03.txt"
 
 ruckSackInput
 // Separate compartments to two char sets
 |> Array.map (fun s -> 
-    s.[..(s.Length/2 - 1)].ToCharArray() |> Set.ofArray,
-    s.[(s.Length/2)..].ToCharArray() |> Set.ofArray) 
+    s.ToCharArray() 
+    |> Array.splitInto 2 
+    |> Array.map Set.ofArray) 
 // Find common letter with intersect and return it
-|> Array.map (fun (set1, set2) -> Set.intersect set1 set2 |> Set.toArray |> Array.head)
+|> Array.map (fun arr-> 
+    Set.intersect  arr.[0] arr.[1] 
+    |> Set.maxElement)
 // Get the value of the letters
-|> Array.map charToIntValue
-|> Array.sum
+|> Array.sumBy charToIntValue
 |> printfn "Answer1: %d"
 
 
@@ -34,18 +32,12 @@ ruckSackInput
 |> Array.map (fun s -> s.ToCharArray())
 // Change arrays to sets for intersect
 |> Array.map Set.ofArray
-|> Seq.ofArray
 // Take three row windows at a time
 |> Seq.chunkBySize 3
 // Find the intersections
 |> Seq.map Set.intersectMany
-// Intersections from sett to array
-|> Seq.map Set.toArray
 // Arrays to chars (the only intersection)
-|> Seq.map Array.head 
-// Sequence to array
-|> Array.ofSeq
+|> Seq.map Set.maxElement 
 // Chars to ints
-|> Array.map charToIntValue
-|> Array.sum
+|> Seq.sumBy charToIntValue
 |> printfn "Answer2: %d"
