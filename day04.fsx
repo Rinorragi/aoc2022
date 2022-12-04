6bfb85885571182cc6fe8cadc11f79ca58347c0f
Rinorragi <rinorragi@gmail.com>
2022-12-04 15:29:36 +0200

    Day4 small refactor

--- day04.fsx
+++ day04.fsx
@@ -4,25 +4,24 @@ open System
 
 let letStringToIntArray (s : string) =
     let arr = s.Split("-",StringSplitOptions.RemoveEmptyEntries)
-    [|Int32.Parse(arr.[0])..Int32.Parse(arr.[1])|]
+    Set [Int32.Parse(arr.[0])..Int32.Parse(arr.[1])]
 
 let elfPairInput = 
     System.IO.File.ReadAllLines "./input/day04.txt"
-    |> Array.map (fun s -> s.Split(',',StringSplitOptions.RemoveEmptyEntries))
+    |> Array.map (fun s -> s.Split(',', StringSplitOptions.RemoveEmptyEntries))
     |> Array.map (fun arr -> 
         (Array.head arr |> letStringToIntArray, 
         Array.last arr |> letStringToIntArray))
 
 elfPairInput
 |> Array.filter (fun (elf1, elf2) ->
-    let intersect = Set.intersect (Set.ofArray elf1) (Set.ofArray elf2)
-    intersect.Count = elf1.Length || intersect.Count = elf2.Length)
+    Set.isSubset elf1 elf2 || Set.isSuperset elf1 elf2)
 |> Array.length
 |> printfn "Answer1: %d"
 
 elfPairInput
 |> Array.filter (fun (elf1, elf2) ->
-    let intersect = Set.intersect (Set.ofArray elf1) (Set.ofArray elf2)
+    let intersect = Set.intersect elf1 elf2
     intersect.Count > 0)
 |> Array.length
-|> printfn "Answer2: %d"
+|> printfn "Answer2: %d" 
