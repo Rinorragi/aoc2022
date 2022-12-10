83a6471605de92f6321ce0c2c7926a3e823f97be
Rinorragi <rinorragi@gmail.com>
2022-12-10 09:51:31 +0200

    Day9 - part 1 refactor

--- day09.fsx
+++ day09.fsx
@@ -4,17 +4,11 @@ exception ValueProblem of string
 type Coordinate = {
     x : int
     y : int
-    tail : Coordinate option
 }
 
 let startPos = {
     x = 0 
     y = 0
-    tail = Some({
-        x = 0
-        y = 0
-        tail = None
-    })
 }
 
 let move instruction x y =
@@ -30,16 +24,12 @@ let tailDirection increaseChar decreaseChar targetValue otherValue =
     | true -> increaseChar
     | false -> decreaseChar
 
-let updatePath path coordinate =
-    path |> List.append [coordinate]
-
 let moveTailX headPos tailX tailY =
     let xDir = tailDirection 'R' 'L' headPos.x tailX
     let xMoved = move xDir tailX tailY
     {
         x = fst xMoved
         y = snd xMoved
-        tail = None
     }
 
 let moveTailY headPos tailX tailY =
@@ -48,7 +38,6 @@ let moveTailY headPos tailX tailY =
     {
         x = fst yMoved
         y = snd yMoved
-        tail = None
     }
 
 let rec moveTail (headPos : Coordinate) (oldTail : Coordinate) =
@@ -74,70 +63,44 @@ let headInput =
     System.IO.File.ReadAllLines "./input/day09.txt"
     |> Array.map (fun s -> (s.[0], s.Substring(2) |> int))
 
-let tailPosition tailSolver headMovement = 
+let tailPosition rope headMovement = 
     headMovement
-    |> Array.fold (fun path (instruction,amount) ->
+    |> Array.fold (fun ropePath (instruction,amount) ->
         [0.. (amount - 1)] 
         |> List.fold (fun (innerPath) _ -> 
-            let head = innerPath |> List.head
+            let oldRope = innerPath |> List.last
+            let head = oldRope |> List.head
             let headPos = move instruction head.x head.y 
             let headNewCoordinate = {
                 x = fst headPos
                 y = snd headPos
-                tail = head.tail
-            }
-            let tailDetails = tailSolver headNewCoordinate
-            let newEntry = {
-                x = headNewCoordinate.x
-                y = headNewCoordinate.y
-                tail = tailDetails
-            }
-            updatePath innerPath newEntry ) 
-            path) [startPos]
-
-let shortTailSolver head = 
-    let newTail = moveTail head head.tail.Value
-    Some({
-        x = newTail.x
-        y = newTail.y
-        tail = None
-    })
-
-let rec popTail times coordinate  = 
-    if times = 0 
-    then 
-        coordinate
-    else
-        match Option.isSome(coordinate.tail) with 
-        | true -> coordinate.tail.Value |> popTail (times - 1)
-        | false -> coordinate
-
-let longTailSolver head =
-    let matchingHead = popTail 8 head
-    let newCoordinate = {
-        x = head.x
-        y = head.y
-        tail = Some({
-            x = matchingHead.x
-            y = matchingHead.y
-            tail = None
-        })
-    } 
-    newCoordinate
-
-headInput 
-|> tailPosition shortTailSolver 
-|> List.filter (fun f -> Option.isSome(f.tail))
-|> List.map (popTail 1)
-|> List.distinct
-|> List.length
-|> printfn "Answer1: %d" 
+            } 
+            let referenceRope = List.append [headNewCoordinate] (oldRope |> List.skip 1)
+            let newTail =
+                oldRope
+                |> List.skip 1
+                |> List.fold (fun (refRope : Coordinate list, newRope) oldTail -> 
+                    let index = newRope |> List.length
+                    let nextTail = moveTail refRope[index] refRope[index + 1]
+                    (refRope, newRope |> List.append [nextTail])
+                ) (referenceRope, [])
+                |> snd
+            let newRope = List.append [headNewCoordinate] newTail
+            List.append innerPath [newRope]
+        ) ropePath
+    ) rope
 
 
-//headInput
-//|> tailPosition longTailSolver 
-//|> List.map List.last
-//|> List.map (fun xy -> (xy.x, xy.y))
-//|> List.distinct
-//|> List.length
-//|> printfn "Answer2: %A" 
+let ropeTailPositionAmount rope =
+    headInput 
+    |> tailPosition rope
+    |> List.map (List.last)
+    |> List.distinct
+    |> List.length
+
+let startList amount = 
+    List.init amount (fun _ -> startPos)
+
+[startList 2] |> ropeTailPositionAmount |> printfn "Answer1: %d" 
+[startList 9] |> ropeTailPositionAmount |> printfn "Answer2 %d" 
+
