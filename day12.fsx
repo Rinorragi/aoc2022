6ee51f856fd5d6a90751be6de52eee88cd80b935
Rinorragi <rinorragi@gmail.com>
2022-12-12 18:27:12 +0200

    Day12 part1

---
+++ day12.fsx
@@ -0,0 +1,111 @@
+#time
+//Dijkstra's algorithm: Nigel Galloway, August 5th., 2018
+//https://rosettacode.org/wiki/Dijkstra%27s_algorithm#F#
+[<CustomEquality;CustomComparison>]
+type Dijkstra<'N,'G when 'G:comparison>=
+    {toN:'N;cost:Option<'G>;fromN:'N}
+    override g.Equals n =match n with| :? Dijkstra<'N,'G> as n->n.cost=g.cost|_->false
+    override g.GetHashCode() = hash g.cost
+    interface System.IComparable with
+        member n.CompareTo g =
+            match g with
+            | :? Dijkstra<'N,'G> as n when n.cost=None -> (-1)
+            | :? Dijkstra<'N,'G>      when n.cost=None -> 1
+            | :? Dijkstra<'N,'G> as g                  -> compare n.cost g.cost
+            | _-> invalidArg "n" "expecting type Dijkstra<'N,'G>"
+
+let inline Dijkstra N G y =
+  let rec fN l f=
+    if List.isEmpty l 
+    then f
+    else 
+        let n=List.min l
+        if n.cost=None 
+        then f 
+        else
+        fN(l|>List.choose(fun n'->
+            if n'.toN=n.toN 
+            then None 
+            else 
+                match n.cost,n'.cost,Map.tryFind (n.toN,n'.toN) G with
+                    |Some g,None,Some wg->Some {toN=n'.toN;cost=Some(g+wg);fromN=n.toN}
+                    |Some g,Some g',Some wg when g+wg<g'->Some {toN=n'.toN;cost=Some(g+wg);fromN=n.toN}
+                    |_ ->Some n'))((n.fromN,n.toN)::f)
+  let r = fN (N|>List.map(fun n->{toN=n;cost=(Map.tryFind(y,n)G);fromN=y})) []
+  (fun n->let rec fN z l=match List.tryFind(fun (_,g)->g=z) r with
+                         |Some(n',g') when y=n'->Some(n'::g'::l)
+                         |Some(n',g') ->fN n' (g'::l)
+                         |_ ->None
+          fN n [])
+
+
+//type Node= |A|B|C|D|E|F
+//let G=Map[((A,B),7);((A,C),9);((A,F),14);((B,C),10);((B,D),15);((C,D),11);((C,F),2);((D,E),6);((E,F),9)]
+
+//printfn "%A" (paths E)
+//printfn "%A" (paths F)
+
+type Coordinate = {
+    id: string
+    x: int
+    y: int
+    height: int
+    start: bool
+    best: bool
+}
+
+let rec charHeight c = 
+    match c with 
+    | 'S' -> charHeight 'a'
+    | 'E' -> charHeight 'z'
+    | _ -> int c - int 'a'
+
+let mazeInput =     
+    System.IO.File.ReadAllLines "./input/day12.txt"
+    |> Array.map Array.ofSeq
+    |> Array.mapi (fun iy row ->
+        row |> Array.mapi (fun ix c -> {
+                id = sprintf "%d_%d" ix iy
+                x = ix
+                y = iy
+                height = charHeight c
+                start = (c = 'S')
+                best = (c = 'E')
+            }))
+
+let fetchCoordinate xy =
+    if fst xy < 0 
+        || fst xy > mazeInput.[0].Length - 1 
+        || snd xy < 0 
+        || snd xy > mazeInput.Length - 1
+    then 
+        None
+    else 
+        Some(mazeInput.[snd xy].[fst xy])
+
+let mazeGraph = 
+    mazeInput
+    |> Array.mapi (fun y row ->
+        row |> Array.mapi (fun x xy -> 
+            let up = fetchCoordinate (x, y + 1) 
+            let down = fetchCoordinate (x, y - 1)
+            let right = fetchCoordinate (x + 1, y)
+            let left = fetchCoordinate (x - 1, y)
+            [(xy,up);(xy,down);(xy,right);(xy,left)] 
+            |> List.filter (snd >> Option.isSome) 
+            |> List.map (fun (xy,other) -> (xy, other.Value))
+            |> List.filter (fun (xy, other) ->  xy.height + 1 >= other.height))
+        |> List.ofArray
+        |> List.concat)
+    |> List.ofArray
+    |> List.concat
+    |> List.map (fun (c1: Coordinate, c2: Coordinate) -> ((c1,c2),1))
+    |> Map.ofList
+
+let mazePoints = mazeInput |> Array.concat |> List.ofArray
+let start = mazePoints |> List.find (fun f -> f.start = true)
+let goal = mazePoints |> List.find (fun f -> f.best = true)
+ 
+let paths=Dijkstra mazePoints mazeGraph start
+let pathFound = (paths goal)
+printfn "%d" (pathFound.Value.Length - 1)
